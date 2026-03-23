18a13efb4195ce9b5198f4f24fbe581a4a85afd9
SudiptoSarkarhih7 <Sudipto.Sarkar@hih7.in>
2026-03-23 15:21:12 +0530

    feat: add root route to confirm server is running

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const app = express();
 app.use(express.json());
 app.use("/api/v1", routes);
 
+// root route
+app.get("/", (req, res) => res.send("Server is running..."));
+
 // Global error handler (must be after routes)
 app.use(errorHandler);
 
